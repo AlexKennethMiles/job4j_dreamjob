8767a37b90d340690b4fc9eb6a8610b6c4a3b5b0
AlexKennethMiles <apuchkov17@gmail.com>
2023-08-20 13:58:57 +0300

    2. CandidateRepository. Хранение кандидатов. [#504861]

--- db/scripts/0002_dml_insert_cities_table.sql
+++ db/scripts/0002_dml_insert_cities_table.sql
@@ -1,3 +1,6 @@
 insert into cities(name) values('Москва');
 insert into cities(name) values('Санкт-Петербург');
+insert into cities(name) values('Новосибирск');
 insert into cities(name) values('Екатеринбург');
+insert into cities(name) values('Казань');
+insert into cities(name) values('Брянск');

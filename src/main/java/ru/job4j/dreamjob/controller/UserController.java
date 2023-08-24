7f166b7ad2d71b36d31d2dab22975953004c54e1
AlexKennethMiles <apuchkov17@gmail.com>
2023-08-24 15:53:03 +0300

    Добавление аннотация для User

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -1,10 +1,14 @@
 package ru.job4j.dreamjob.controller;
 
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.UserService;
 
+@ThreadSafe
+@Controller
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
